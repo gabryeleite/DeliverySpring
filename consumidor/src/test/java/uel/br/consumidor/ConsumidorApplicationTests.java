325cbbe44a2fb79fe25f49afc1093f338daa8e26
Gabryel Leite <96426826+gabryeleite@users.noreply.github.com>
2024-04-25 18:09:44 -0300

    Add files via upload

---
+++ consumidor/src/test/java/uel/br/consumidor/ConsumidorApplicationTests.java
@@ -0,0 +1,13 @@
+package uel.br.consumidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
